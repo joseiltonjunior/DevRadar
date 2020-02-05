320868b7064dac6f43c543b6b618fa3a5d732ace
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-02-04 22:44:31 -0300

    add input form and buttom

--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function Profile() {
-  return <View />
+function Profile({ navigation }) {
+  const githubUser = navigation.getParam('github_username');
+
+  return <WebView style={{ flex: 1 }} source={{ uri: `http://github.com/${githubUser}` }} />
 }
 
 export default Profile;
